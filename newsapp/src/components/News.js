8060688ddea76cc42974357d835c28ccfcc91bef
ronak <ronak04.exhibyte@gmail@gmail.com>
2021-10-14 16:12:08 +0530

    infinite code

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Newsiteam from "./Newsiteam";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 export class News extends Component {
   static defaultProps = {
     country: "in",
@@ -22,15 +23,16 @@ export class News extends Component {
     super(props);
     this.state = {
       artical: [],
-      loading: false,
+      loading: true,
       page: 1,
+      totalResults: 0,
     };
     document.title = ` ${this.capitalizeFirstLetter(
       this.props.category
     )}-Newsapp`;
   }
-  async UpdateNews() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=085728e13a314f85aef565032d9b02be&page=1&pagesize=${this.props.pagesize}`;
+  async update() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3ec2275ce9d5439dbd6223d33fba8663&page=${this.state.page}&pagesize=${this.props.pagesize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -41,18 +43,19 @@ export class News extends Component {
       loading: false,
     });
   }
+
   async componentDidMount() {
-    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=085728e13a314f85aef565032d9b02be&page=1&pagesize=${this.props.pagesize}`;
-    // this.setState({ loading: true });
-    // let data = await fetch(url);
-    // let parsedData = await data.json();
-    // console.log(parsedData);
-    // this.setState({
-    //   artical: parsedData.articles,
-    //   totalResults: parsedData.totalResults,
-    //   loading: false,
-    // });
-    this.UpdateNews();
+    //   let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=085728e13a314f85aef565032d9b02be&page=1&pagesize=${this.props.pagesize}`;
+    //   this.setState({ loading: true });
+    //   let data = await fetch(url);
+    //   let parsedData = await data.json();
+    //   console.log(parsedData);
+    //   this.setState({
+    //     artical: parsedData.articles,
+    //     totalResults: parsedData.totalResults,
+    //     loading: false,
+    //   });
+    this.update();
   }
 
   handlepreClick = async () => {
@@ -74,7 +77,7 @@ export class News extends Component {
     //   loading: false,
     // });
     this.setState({ page: this.state.page - 1 });
-    this.UpdateNews();
+    this.update();
   };
 
   handlenexClick = async () => {
@@ -102,7 +105,20 @@ export class News extends Component {
     //   });
     // }
     this.setState({ page: this.state.page + 1 });
-    this.UpdateNews();
+    this.update();
+  };
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3ec2275ce9d5439dbd6223d33fba8663&page=${this.state.page}&pagesize=${this.props.pagesize}`;
+    // this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      artical: this.state.artical.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      // loading: false,
+    });
   };
 
   render() {
@@ -115,28 +131,43 @@ export class News extends Component {
           </span>
         </h1>
         {this.state.loading && <Spinner />}
-
-        <div className="row">
-          {!this.state.loading &&
-            this.state.artical.map((e) => {
-              return (
-                <div className="col-md-4" key={e.url}>
-                  <Newsiteam
-                    title={e.title ? e.title.slice(0, 45) : ""}
-                    description={
-                      e.description ? e.description.slice(0, 80) : ""
-                    }
-                    urlToImage={e.urlToImage}
-                    newsurl={e.url}
-                    author={e.author}
-                    date={e.publishedAt}
-                    Info={e.source.name}
-                  />
-                </div>
-              );
-            })}
-        </div>
-        <div className="container d-flex justify-content-between">
+        <InfiniteScroll
+          dataLength={this.state.artical.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.artical.length !== this.state.totalResults}
+          loader={
+            <h4>
+              {" "}
+              <Spinner />
+            </h4>
+          }
+        >
+          <div className="container">
+            <div className="row">
+              {
+                // !this.state.loading &&
+                this.state.artical.map((e) => {
+                  return (
+                    <div className="col-md-4" key={e.url}>
+                      <Newsiteam
+                        title={e.title ? e.title.slice(0, 45) : ""}
+                        description={
+                          e.description ? e.description.slice(0, 80) : ""
+                        }
+                        urlToImage={e.urlToImage}
+                        newsurl={e.url}
+                        author={e.author}
+                        date={e.publishedAt}
+                        Info={e.source.name}
+                      />
+                    </div>
+                  );
+                })
+              }
+            </div>
+          </div>
+        </InfiniteScroll>
+        {/* <div className="container d-flex justify-content-between">
           <button
             disabled={this.state.page <= 1}
             className="btn btn-dark"
@@ -156,7 +187,7 @@ export class News extends Component {
           >
             Next &rarr;
           </button>
-        </div>
+        </div> */}
       </div>
     );
   }
